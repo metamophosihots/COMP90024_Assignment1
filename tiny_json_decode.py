b9a6905d2c6a91f748d1579ca161cf237cdf20fa
49 <645487820@qq.com>
2021-03-20 10:57:10 +0800

    fix minor encoding problem

--- tiny_json_decode.py
+++ tiny_json_decode.py
@@ -1,4 +1,4 @@
 import json
-with open("tinyTwitter.json", "r") as read_file:
+with open("tinyTwitter.json", "r",encoding='UTF-8') as read_file:
     data = json.load(read_file)
 print(json.dumps(data, indent=4, sort_keys=True))
