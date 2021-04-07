7757cd052f3e790af16ae741c27248041c7e0718
metamophosihots <75021917+metamophosihots@users.noreply.github.com>
2021-04-07 17:54:08 +1000

    Add files via upload

--- Test File/tinyTwitter_test_demo.py
+++ Test File/tinyTwitter_test_demo.py
@@ -51,12 +51,15 @@ def seizeTwitterContent(twitter_file):
 
 def calculateLongitudeZone(coordinates):
     longitude = coordinates[0]
-    longitude_district = (longitude - 144.7) / 0.15
-    if longitude_district < 0 or longitude_district > 5:
+    relative_longitude = format((longitude - 144.7), '.8f')
+    relative_longitude = float(relative_longitude)
+    print(relative_longitude)
+    if relative_longitude < 0 or relative_longitude > 0.6:
         # Stop searching this twitter
         longitude_zone = -1
     else:
-        if 0 <= longitude_district <= 1:
+        longitude_district = relative_longitude / 0.15
+        if longitude_district <= 1:
             longitude_zone = 1
         elif longitude_district <= 2:
             longitude_zone = 2
@@ -71,16 +74,19 @@ def calculateLongitudeZone(coordinates):
 
 def calculateLatitudeZone(coordinates):
     latitude = coordinates[1]
-    latitude_district = (latitude + 38.1) / 0.15
-    if latitude_district < 0 or latitude_district > 4:
+    relative_latitude = format((latitude + 38.1), '.8f')
+    relative_latitude = float(relative_latitude)
+    print(relative_latitude)
+    if relative_latitude < 0 or relative_latitude > 0.6:
         # Stop searching this twitter
         latitude_zone = 'X'
     else:
-        if 0 <= latitude_district < 1:
+        latitude_district = relative_latitude / 0.15
+        if latitude_district <= 1:
             latitude_zone = 'D'
-        elif latitude_district < 2:
+        elif latitude_district <= 2:
             latitude_zone = 'C'
-        elif latitude_district < 3:
+        elif latitude_district <= 3:
             latitude_zone = 'B'
         else:
             latitude_zone = 'A'
@@ -89,41 +95,12 @@ def calculateLatitudeZone(coordinates):
 
 # judge if the zone is one of the zones in melbGrid
 def allocateZone(longitude_zone, latitude_zone):
-    if longitude_zone == 1 and latitude_zone == 'A':
-        twitter_zone = 'A1'
-    elif longitude_zone == 2 and latitude_zone == 'A':
-        twitter_zone = 'A2'
-    elif longitude_zone == 3 and latitude_zone == 'A':
-        twitter_zone = 'A3'
-    elif longitude_zone == 4 and latitude_zone == 'A':
-        twitter_zone = 'A4'
-    elif longitude_zone == 1 and latitude_zone == 'B':
-        twitter_zone = 'B1'
-    elif longitude_zone == 2 and latitude_zone == 'B':
-        twitter_zone = 'B2'
-    elif longitude_zone == 3 and latitude_zone == 'B':
-        twitter_zone = 'B3'
-    elif longitude_zone == 4 and latitude_zone == 'B':
-        twitter_zone = 'B4'
-    elif longitude_zone == 1 and latitude_zone == 'C':
-        twitter_zone = 'C1'
-    elif longitude_zone == 2 and latitude_zone == 'C':
-        twitter_zone = 'C2'
-    elif longitude_zone == 3 and latitude_zone == 'C':
-        twitter_zone = 'C3'
-    elif longitude_zone == 4 and latitude_zone == 'C':
-        twitter_zone = 'C4'
-    elif longitude_zone == 5 and latitude_zone == 'C':
-        twitter_zone = 'C5'
-    elif longitude_zone == 3 and latitude_zone == 'D':
-        twitter_zone = 'D3'
-    elif longitude_zone == 4 and latitude_zone == 'D':
-        twitter_zone = 'D4'
-    elif longitude_zone == 5 and latitude_zone == 'D':
-        twitter_zone = 'D5'
+    twitter_zone_list = ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C1', 'C2', 'C3', 'C4', 'C5', 'D3', 'D4', 'D5']
+    twitter_zone = latitude_zone + str(longitude_zone)
+    if twitter_zone in twitter_zone_list:
+        return twitter_zone
     else:
-        twitter_zone = 'X0'
-    return twitter_zone
+        return 'X0'
 
 
 # Transform twitter text into all lower case situation, and then split them with all possible mark
